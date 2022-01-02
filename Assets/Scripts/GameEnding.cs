72ed5d46a90d33742cd0801975c31d12c1f5835b
xidsf <gooderic816053@gmail.com>
2022-01-03 01:34:53 +0900

    그래픽, 유령, 가고일 등 설치, 보이스 미구현

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    private float displayImageDuration = 2f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+
+    private bool m_IsPlayerAtExit = false;
+    bool m_IsPlayerCaught = false;
+    private float m_Timer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    private void Update()
+    {
+        if(m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+        }
+    }
+
+    private void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    {
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            if(doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    public float turnSpeed = 20f;
+
+    Rigidbody m_rigidbody;
+    Animator m_Animator;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_rigidbody.MovePosition(m_rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_rigidbody.MoveRotation(m_Rotation);
+    }
+}
